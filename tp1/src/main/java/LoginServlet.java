2b9536fdbfd7ba0ac31a8198bae6f42120d14919
JaimeR-123 <jaimeferroroque@gmail.com>
2026-06-07 11:29:31 +0100

    adicionado login e logout servlet

---
+++ tp1/src/main/java/LoginServlet.java
@@ -0,0 +1,36 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nick = request.getParameter("nickname");
+		String pass = request.getParameter("password");
+		String xmlPath = (String) getServletContext.getAttribute("xmlPath");
+		
+		try {
+			org.w3c.dom.Document doc = XMLReader.loadXML(xmlPath);
+			
+			if (XMLReader.validarLogin(doc, nick, pass)) {
+				HttpSession session = request.getSession();
+				session.setAttribute("user", nick);
+				
+				response.sendRedirect("lobby.jsp");
+			} else {
+				request.setAttribute("erro", "Credenciais inválidas");
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+		
+	}
+}
